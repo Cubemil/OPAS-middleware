3d92312d0e9cfa4b11608fd388cffeb6b6ff37ae
Petersen, Emil Justus <emiljustus.petersen@student.hs-anhalt.de>
2024-06-18 16:00:56 +0200

    added RecordId, split house numbers, removed description in model

--- src/Models/OffenseRecord.cs
+++ src/Models/OffenseRecord.cs
@@ -1,11 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace src.Models
 {
     public class OffenseRecord
     {
 
-        [Key]        
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int RecordId { get; set; }
         public string Aktenzeichen { get; set; } = string.Empty;
         public string Anrede { get; set; } = string.Empty;
         public string Vorname { get; set; } = string.Empty;
@@ -15,10 +18,10 @@ namespace src.Models
         public string Plz { get; set; } = string.Empty;
         public string Wohnort { get; set; } = string.Empty;
         public string Str { get; set; } = string.Empty;
-        public string Hausnummer { get; set; } = string.Empty;
+        public string HausnummerInt { get; set; } = string.Empty;
+        public string HausnummerExtra { get; set; } = string.Empty;
         public string Versicherungsnummer { get; set; } = string.Empty;
         public string Krankenversicherungsname { get; set; } = string.Empty;
         public string Vertragsunternehmensnummer { get; set; } = string.Empty;
-        public string Beschreibung { get; set; } = string.Empty;
     }
 }
--- src/OffenseDbContext.cs
+++ src/OffenseDbContext.cs
@@ -5,12 +5,20 @@ using src.Models;
 // represents tables as DbSet properties (entity framework)
 public class OffenseDbContext : DbContext
 {
-    public OffenseDbContext(DbContextOptions<OffenseDbContext> options) : base(options)
-    {
-
-    }
 
     public DbSet<OffenseRecord> OffenseRecords { get; set; }
+
+    public OffenseDbContext(DbContextOptions<OffenseDbContext> options) : base(options) {}
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // autoamtically adds RecordId to record when added to db
+        modelBuilder.Entity<OffenseRecord>()
+            .Property(e => e.RecordId)
+            .ValueGeneratedOnAdd();
+
+        base.OnModelCreating(modelBuilder);
+    }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     {
